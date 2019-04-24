e3cf471d16e92d8d40806bb09c4217c3944e4501
Peter Petkanic <crunckles@gmail.com>
2019-04-24 12:53:53 +0200

    added offer data model

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -22,3 +23,32 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+class Offer(models.Model):
+    RICE = 'RE'
+    COTTON = 'CN'
+    SORGHUM = 'SM'
+    SEED_TYPES_CHOICES = (
+        (RICE, 'Rice'),
+        (COTTON, 'Cotton'),
+        (SORGHUM, 'Sorghum'),
+    )
+    seed_type = models.CharField(
+        max_length=2,
+        choices=SEED_TYPES_CHOICES,
+        default=RICE,
+    )
+    amount_of_seeds = models.IntegerField(
+        default=1,
+        validators=[
+            MaxValueValidator(999),
+            MinValueValidator(1)
+        ]
+    )
+    days_online = models.IntegerField(
+        default=1,
+        validators=[
+            MaxValueValidator(90),
+            MinValueValidator(1)
+        ]
+    )
